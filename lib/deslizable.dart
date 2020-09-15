37850a99e422490742c699fda4bd2e660f716c2c
JUANITOTELO <jdmm.jdmm31@gmail.com>
2020-09-15 16:05:30 -0500

    Primer botón conectado a Firebase

--- lib/deslizable.dart
+++ lib/deslizable.dart
@@ -1,16 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class Deslizable extends StatefulWidget {
-  const Deslizable({Key key}) : super(key: key);
-
+  Deslizable({this.app});
+  final FirebaseApp app;
   @override
   _DeslizableState createState() => _DeslizableState();
 }
 
 class _DeslizableState extends State<Deslizable> {
+  DatabaseReference _led1Ref;
+
   Color color1 = Colors.grey;
   Color color2 = Colors.cyan;
   bool m = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _led1Ref = FirebaseDatabase.instance.reference().child('led1');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -47,9 +58,11 @@ class _DeslizableState extends State<Deslizable> {
                                 if (m == false) {
                                   color1 = color2;
                                   m = true;
+                                  _led1Ref.set(1);
                                 } else {
                                   color1 = Colors.grey;
                                   m = false;
+                                  _led1Ref.set(0);
                                 }
                               });
                             },
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'deslizable.dart';
 
-void main() {
-  runApp(Deslizable());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final FirebaseApp app = await Firebase.initializeApp(
+    name: "domoticc",
+    options: FirebaseOptions(
+        apiKey: "AIzaSyAPOFoXfKnGl-Y-utGSRF4XeeFcMZkSfE8",
+        authDomain: "macc-domoticc.firebaseapp.com",
+        databaseURL: "https://macc-domoticc.firebaseio.com",
+        projectId: "macc-domoticc",
+        storageBucket: "macc-domoticc.appspot.com",
+        messagingSenderId: "852333646886",
+        appId: "1:852333646886:android:b7fb468b56eb00246202e7",
+        measurementId: "G-SD848QB5QP"),
+  );
+  runApp(Deslizable(
+    app: app,
+  ));
 }
